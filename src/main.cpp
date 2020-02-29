730fc983b8c0ce8493e457802e4c32a57f9848c9
Vladlen <wertizator@rambler.ru>
2020-02-29 12:24:54 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,11 @@
 #include <Arduino.h>
-
-#define PIN 3
-
-// Подключаем библиотеку Adafruit NeoPixel.
 #include <Adafruit_NeoPixel.h>
 
-// Указываем, какое количество пикселей у нашей ленты.
+#define LED 13
 #define LED_COUNT 6
-
-// Указываем, к какому порту подключен вход ленты DIN.
 #define LED_PIN 5
 
-// Создаем переменную strip для управления нашей лентой.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
-int myColor = 0;
 
 void offLed()
 {
@@ -23,94 +14,30 @@ void offLed()
   strip.show();
 }
 
-void seLed(int ledPin)
-{
-  switch (ledPin)
-  {
-  case 0:
-    strip.setPixelColor(0, strip.Color(255, 0, 0));
-    strip.setPixelColor(1, strip.Color(255, 0, 0));
-    break;
-  case 1:
-    strip.setPixelColor(0, strip.Color(0, 255, 0));
-    strip.setPixelColor(1, strip.Color(0, 255, 0));
-    break;
-  case 2:
-    strip.setPixelColor(0, strip.Color(0, 0, 255));
-    strip.setPixelColor(1, strip.Color(0, 0, 255));
-
-    break;
-
-  default:
-    break;
-  }
-  strip.show();
-}
-
 void setup()
 {
-  // Инициализируем ленту.
   strip.begin();
-
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-  seLed(myColor);
 
   if (Serial.available() > 0)
-  { //если есть доступные данные
-    // считываем байт
+  {
     int incomingByte = Serial.read();
-    if (incomingByte == 49)
+
+    if (incomingByte == '1')
     {
-      myColor = 0;
+      digitalWrite(LED, HIGH);
     }
 
-    if (incomingByte == 50)
+    if (incomingByte == '2')
     {
-      myColor = 1;
     }
-    if (incomingByte == 51)
+    if (incomingByte == '3')
     {
-      myColor = 2;
     }
   }
-}
-
-//strip.setPixelColor(1, strip.Color(0, 255, 0)); // Красный цвет.
-
-//    if (Serial.available() > 0) {  //если есть доступные данные
-//        // считываем байт
-//        int incomingByte = Serial.read();
-//
-//        if(incomingByte == 49)
-//        {
-//          digitalWrite(PIN,HIGH);
-//        }
-//
-//        if(incomingByte == 50)
-//        {
-//          digitalWrite(PIN,LOW);
-//        }
-//    }
-
-//digitalWrite(PIN,HIGH);
-//delay(1000);
-//digitalWrite(PIN,LOW);
-//delay(1000);
-//
-//for(int i=0;i<5;i++)
-//{
-//  isLight();
-//}
-//
-
-void isLight()
-{
-  digitalWrite(PIN, HIGH);
-  delay(100);
-  digitalWrite(PIN, LOW);
-  delay(100);
 }
